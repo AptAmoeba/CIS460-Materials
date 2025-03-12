487e51262657b6f0dbc3089c4550221be3368b61
AptAmoeba <adalsing1102@gmail.com>
2025-03-12 00:37:22 -0500

    Update HavocC2-Helper.sh
    Added color and adjusted directory naming.

--- Havoc C2 Framework/HavocC2-Helper.sh
+++ Havoc C2 Framework/HavocC2-Helper.sh
@@ -3,13 +3,15 @@
 # === THIS SCRIPT REQUIRES SUDO PERMISSIONS TO RUN! ===
 # This script installs dependencies for the Havoc C2 Framework and deploys the Teamserver & Client. 
 # To execute the Havoc Client interface after installation, please enter the /Havoc directory and run the following command:
-# ./havoc client
+# sudo ./havoc client
 #
 # Download this script and execute it in your Desktop with sudo permissions.
 # 
-###
+RED="\033[1;31m"
+NOCOLOR="\033[0m"
+### Banner
 echo ""
-echo '_____________________________________________ '
+echo -e "_____________________________________________${RED}"
 echo '          _______           _______  _______ '
 echo '|\     /|(  ___  )|\     /|(  ___  )(  ____ \'
 echo '| )   ( || (   ) || )   ( || (   ) || (    \/'
@@ -18,7 +20,8 @@ echo '|  ___  ||  ___  |( (   ) )| |   | || |      '
 echo '| (   ) || (   ) | \ \_/ / | |   | || |      '
 echo '| )   ( || )   ( |  \   /  | (___) || (____/\'
 echo '|/     \||/     \|   \_/   (_______)(_______/'
-echo '============== INSTALLER SCRIPT ============== '
+echo -e "${NOCOLOR}============== ${RED}INSTALLER SCRIPT${NOCOLOR} =============="
+echo -e ${NOCOLOR}
 ###
 echo "Verifying sudo permissions..."
 if [ "$EUID" -ne 0 ]
@@ -28,8 +31,8 @@ fi
 echo ""
 echo "[+] Creating Havoc Setup directory..."
 sleep 2
-mkdir Havoc-Setup
-cd Havoc-Setup
+mkdir Havoc-Demo
+cd Havoc-Demo
 #Cloning into Havoc Github
 echo "[+] Cloning Havoc C2 github contents into our local setup directory..."
 git clone https://github.com/HavocFramework/Havoc.git
@@ -47,8 +50,11 @@ sudo apt install libdrm2
 sudo apt install libdrm2-intel1
 sudo apt install libdrm2-dev
 ### End primary dependencies for Havoc
+echo 1
 echo ""
-echo "[+] BE PATIENT. This next section will take a moment. It won't show anything on-screen, but it's building a lot in the background. Just let it do its thing."
+echo "=== ATTENTION ==="
+echo -e "[+] ${RED}BE PATIENT.${NOCOLOR} This next section will take a moment. It won't show anything on-screen, but it's building a lot in the background. Just let it do its thing."
+echo "================="
 sleep 4
 echo ""
 cd teamserver/
@@ -70,18 +76,25 @@ echo ''
 sudo make client-build
 echo ""
 echo "[+] Finished!"
-echo "[NOTE] Changed directory to /Havoc directory. Havoc commands will need to be run within this directory to succeed."
+echo "[NOTE] Change directory to /Havoc directory. Havoc commands will need to be run within this directory to succeed."
+sleep 2
 echo ""
 echo "=== Connection information ==="
+sleep .2
 echo "- Name: CIS460 TeamServer"
+sleep .2
 echo "- Host: 172.16.0.165"
+sleep .2
 echo "- Port: 40056"
+sleep .2
 echo "- User: (Given in class)"
+sleep .2
 echo "- Pass: (Given in class)"
 echo "=============================="
+sleep 1
 echo ""
 echo "[+] To launch your Havoc C2 interactive client, run the following command:"
-echo "'./havoc client'"
+echo "'sudo ./havoc client'"
 echo ""
 echo "[NOTE]Your professor manages whitelisted users. He will give you your username & password for login. Please wait!"
 echo "[NOTE] By the way, everything everyone does in the Teamserver is recorded via logging!"
